3798e88223be738f39cdd39b4aecd5f3c2c59c98
André Luis <andre.andrade@madeiramadeira.com.br>
2022-04-13 11:41:37 -0300

    alterando background e tirando bugs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import { styled } from '@stitches/react';
 import './App.css';
 
-const Button = styled('button', {});
+const Button = styled('button', {
+  backgroundColor: 'gainsboro',
+  borderRadius: '9999px',
+  fontSize: '13px',
+  padding: '10px 15px',
+  '&:hover': {
+    backgroundColor: 'gray',
+    cursor: 'pointer',
+  },
+});
 
 function App(){
     return (
